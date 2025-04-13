aff0cd686893b009b450c4bb05dc2adb93b1fa07
T S Ajeet <ajeet.tumuluru@gmail.com>
2025-04-13 08:50:22 +0530

    telescope, treesitter

--- init.lua
+++ init.lua
@@ -29,7 +29,8 @@ require('lazy').setup({
     require "plugins.colortheme",
     require "plugins.bufferline",
     require "plugins.lualine",
-    require "plugins.treesitter"
+    require "plugins.treesitter",
+    require "plugins.telescope"
 })
 
 
